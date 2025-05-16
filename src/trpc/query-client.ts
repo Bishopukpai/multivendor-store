5c936a8e1e729b36e72f09a2020da133b355b01e
Bishopukpai <bishopukpai0@gmail.com>
2025-05-16 14:18:43 +0100

    07 fixed

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -1,5 +1,5 @@
 import {defaultShouldDehydrateQuery, QueryClient} from '@tanstack/react-query';
-// import superjson from 'superjson';
+import superjson from 'superjson';
 
 export function makeQueryClient() {
     return new QueryClient({
@@ -8,10 +8,11 @@ export function makeQueryClient() {
                 staleTime: 30 * 1000,
             },
             dehydrate: {
+                serializeData: superjson.serialize,
                 shouldDehydrateQuery: (query) => defaultShouldDehydrateQuery(query) || query.state.status === 'pending',
             },
             hydrate: {
-
+                deserializeData: superjson.deserialize
             }
         }
     })
